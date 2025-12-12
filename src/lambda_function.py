08608678399e622a300763171f9b6326e2c1f1e3
dknayak1309 <dknayak1309@gmail.com>
2025-12-12 22:45:18 +0530

    retest

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! From DK')
+        'body': json.dumps('Hello from Lambda! From DK date 12_12_2024')
     }
